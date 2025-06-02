ef8b1650d6758dca926b35688615a82a1fea6d22
PaulDL <paul.durand-lupinski@reeverse-systems.com>
2025-06-02 10:02:36 +0200

    bump version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'spyrrow'
 copyright = '2025, Paul Durand-Lupinski'
 author = 'Paul Durand-Lupinski'
-release = '0.3.0'
+release = '0.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
